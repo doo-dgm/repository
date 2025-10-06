f23b4382c9705e78ce5159efe1f90203e6fee18a
Michelgdr28 <michel.guarnizo@udea.edu.co>
2025-10-06 11:20:19 -0500

    Actualizacion CountryDomain
    Carga Clase CountryDomain Domain

--- TreePruning/src/main/java/co/edu/uco/treepruning/business/domain/CountryDomain.java
+++ TreePruning/src/main/java/co/edu/uco/treepruning/business/domain/CountryDomain.java
@@ -1,5 +1,32 @@
 package co.edu.uco.treepruning.business.domain;
 
-public class CountryDomain {
+import java.util.UUID;
+import co.edu.uco.treepruning.crosscuting.helper.TextHelper;
+import co.edu.uco.treepruning.crosscuting.helper.UUIDHelper;
 
+public final class CountryDomain extends Domain {
+	private String name;
+	
+	public CountryDomain() {
+		super (UUIDHelper.getUUIDHelper().getDefault());
+		setName(TextHelper.getDefault());
+	}
+	
+	public CountryDomain(final UUID id) {
+		super (id);
+		setName (TextHelper.getDefault());
+	}
+	
+	public CountryDomain(final UUID id,final String name) {
+		super(id);
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName (final String name) {
+		this.name = TextHelper.getDefaultWithTrim(name);
+	}
 }
+
