7b3af801708b59e3559b00bf6526da876706bf39
zha808 <daniloc90@gmail.com>
2025-10-31 22:39:44 -0500

    AdministratorDAOfactory

--- TreePruning/src/main/java/co/edu/uco/treepruning/business/business/impl/AdministratorBusinessImpl.java
+++ TreePruning/src/main/java/co/edu/uco/treepruning/business/business/impl/AdministratorBusinessImpl.java
@@ -3,9 +3,15 @@ package co.edu.uco.treepruning.business.business.impl;
 import java.util.UUID;
 
 import co.edu.uco.treepruning.business.business.AdministratorBusiness;
+import co.edu.uco.treepruning.data.dao.factory.DAOFactory;
 
 public class AdministratorBusinessImpl implements AdministratorBusiness {
-
+	
+	private DAOFactory daoFactory;
+	
+	public AdministratorBusinessImpl(final DAOFactory daoFactory) {
+		this.daoFactory = daoFactory;
+	}
 	@Override
 	public void findAllAdministrator() {
 		// TODO Auto-generated method stub
